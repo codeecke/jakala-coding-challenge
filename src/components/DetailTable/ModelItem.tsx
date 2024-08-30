566101e1dc4d04a66cae862529fb7793214972e2
Michael Hammer <michaelhammer84@gmail.com>
2024-08-30 22:17:25 +0200

    Fallback for ModelItem implemented

--- src/components/DetailTable/ModelItem.tsx
+++ src/components/DetailTable/ModelItem.tsx
@@ -2,5 +2,8 @@ import React, { FC } from 'react'
 import { AbstractModel } from '../../models/AbstractModel'
 
 export const ModelItem: FC<{ model: AbstractModel }> = ({ model }) => {
+  if(!model.getDetailLink) {
+    return model.toString()
+  }
   return <a href={model.getDetailLink()}>{model.toString()}</a>
 }
